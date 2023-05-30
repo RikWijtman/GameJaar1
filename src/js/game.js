5bb7677c9d298a28b12779661d13927a9a209710
RikWijtman <1045768@hr.nl>
2023-05-30 14:01:46 +0200

    game

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
 import { Actor, Engine, Vector } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { getPosition } from 'excalibur/build/dist/Util/Util'
 
 export class Game extends Engine {
 
@@ -11,11 +12,16 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
+        const fish = new Actor({
+            width: Resources.Fish.width,
+            height: Resources.Fish.height
+        })
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(400, 300)
         fish.vel = new Vector(-10,0)
         this.add(fish)
+
+        
     }
 }
 
